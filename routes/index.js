98f3587fe76cf4ed349d95da0ca75c74b8fe9e38
sx-9245 <sx-chenr@dtdream.com>
2017-03-17 14:10:53 +0800

    利用 superagent 和 cheerio 简单完成爬虫功能

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,31 @@
 var express = require('express');
+var superagent = require('superagent');
+var cheerio = require('cheerio');
+
 var router = express.Router();
 
 router.get('/', function(req, res, next){
-    res.render('index', {
-        name: 'crcrcry',
-        msg: 'start'
-    });
+    superagent.get('https://cnodejs.org/')
+        .end(function(err, sres){
+            if(err){
+                res.send("Get Request Error!");
+            }
+
+            var $ = cheerio.load(sres.text);
+            var items = [];
+
+            $('#topic_list .topic_title').each(function(idx, element){
+                var $element = $(element);
+                items.push({
+                    title: $element.attr('title'),
+                    href: $element.attr('href')
+                })
+            })
+
+            res.render('index', {
+                news: items
+            })
+        })
 })
 
 module.exports = router;
